1323e04c73ef937c6f30a43509adc51e4edad88b
edubew <edubew98@gmail.com>
2023-07-04 19:35:46 +0300

    Modify the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  resources :categories, only: [:index, :new, :create] do
+    resources :records, only: [:index, :new, :create]
+  end
+  
+
+  devise_for :users, skip: [:sessions]
+
+  as :user do
+    get 'login', to: 'sessions#new', as: :new_user_session
+    post 'login', to: 'sessions#create', as: :user_session
+    delete 'logout', to: 'sessions#destroy', as: :destroy_user_session
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'splash#splash'
 end
