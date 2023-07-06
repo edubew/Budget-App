cd2f2e4bbc321877e0cb4fcd20bbeb4d925202de
edubew <edubew98@gmail.com>
2023-07-06 08:35:59 +0300

    Add a method to calculate amount

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,12 @@
 class Category < ApplicationRecord
+    self.table_name = 'categories'
     belongs_to :author, class_name: 'User'
     has_and_belongs_to_many :records, dependent: :destroy
 
     validates :name, presence: true
     validates :icon, presence: true
+
+    def total_amount
+        records.sum(:amount)
+    end
 end
