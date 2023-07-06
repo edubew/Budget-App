e84ce30460bc1f3dced1864e5a077655f0e31218
edubew <edubew98@gmail.com>
2023-07-06 08:40:16 +0300

    Modify the controllers new and destroy methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,34 @@
 class CategoriesController < ApplicationController
+    before_action :authenticate_user!, only: [:index]
+
     def index
-        @categories = Category.all
+        @categories = Category.where(author_id: current_user.id)
     end
 
     def new
         @category = Category.new
     end
 
+    def create
+        @category = current_user.categories.new(category_params)
+
+        if @category.save
+            redirect_to categories_path, notice: 'Category was successfully created!!'
+        else
+            @errors = @category.errors.full_messages
+            render :new, alert: @errors
+        end
+    end
+
     def destroy
-        category = Category.find(params[:id])
-        category.destroy
-        redirect_to categories_path, notice: "Deleted Category: #{category.name}"
+        @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to categories_path, notice: "Deleted Category: #{@category.name}"
+    end
+
+    private
+
+    def category_params
+      params.require(:category).permit(:name, :icon)
     end
 end
